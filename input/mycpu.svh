2b28d5b6f45270956ec7310929bf57507be59e1b
Heikki Marjala <heikki.marjala@student.oulu.fi>
2022-11-23 11:59:28 +0200

    Add pc rtl, trim whitespaces

--- input/mycpu.svh
+++ input/mycpu.svh
@@ -9,11 +9,11 @@
 //`define ENABLE_CPU_REPORTER
 
 // This macro creates an unknowns check assertion
-`define xcheck(name) X_``name``: assert property ( @(posedge clk) disable iff (rst_n !== '1) !$isunknown( name) ) else $error(`"name has unknown bits.`")   
+`define xcheck(name) X_``name``: assert property ( @(posedge clk) disable iff (rst_n !== '1) !$isunknown( name) ) else $error(`"name has unknown bits.`")
 
 // This macro creates an reset check assertion
-`define resetcheck(name,value) R_``name``: assert property ( @(posedge rst_n) (name == value) ) else $error(`"name reset state is wrong.`")   
-		
+`define resetcheck(name,value) R_``name``: assert property ( @(posedge rst_n) (name == value) ) else $error(`"name reset state is wrong.`")
+
 `endif
 
 
