5b1ed82fc11c629acedac51c246e18ddf0269497
Heikki Marjala <heikki.marjala@student.oulu.fi>
2022-12-06 12:37:12 +0200

    Add rb test

--- input/rb_tb.sv
+++ input/rb_tb.sv
@@ -12,14 +12,14 @@ module rb_tb;
    logic [11:0] 	 rs_in;
    logic [15:0] 	 a_out;
    logic [15:0] 	 b_out;
-   
+
    rb DUT_INSTANCE (.*);
    rb_test TEST (.*);
 
 `ifdef RTL_SIM
- `include "mycpu_svabind.svh"   
-`endif 
-   
+ `include "mycpu_svabind.svh"
+`endif
+
    always
      begin : clock_generator
 	if (clk === '0)
--- input/rb_test.sv
+++ input/rb_test.sv
@@ -4,32 +4,60 @@ import mycpu_pkg::*;
 
 program rb_test
   (
-   input logic 		 clk,
-   input logic 		 rst_n,
-   output logic [15:0] d_in,
-   output logic 	 rw_in,
-   output logic [11:0] 	 rs_in,
-   input logic [15:0]  a_out,
-   input logic [15:0]  b_out
-   );
+  input logic 		 clk,
+  input logic 		 rst_n,
+  output logic [15:0] d_in,
+  output logic 	 rw_in,
+  output logic [11:0] 	 rs_in,
+  input logic [15:0]  a_out,
+  input logic [15:0]  b_out
+  );
 
-   logic [15:0] 	 patterns [16];
-   
-   initial
-     begin
-	///////////////////////////////////////////////////////////////////////
-	$info("T1: Reset Test");
-	///////////////////////////////////////////////////////////////////////	
-	d_in = '0;
-	rw_in = '0;
-	rs_in = '0;
-	wait (rst_n);
+  logic [15:0] 	 patterns [16];
 
-	///////////////////////////////////////////////////////////////////////	
-	$info("T2: Read-Write Test");
-	///////////////////////////////////////////////////////////////////////
+  initial
+  begin
+  ///////////////////////////////////////////////////////////////////////
+  $info("T1: Reset Test");
+  ///////////////////////////////////////////////////////////////////////
+  d_in = '0;
+  rw_in = '0;
+  rs_in = '0;
+  wait (rst_n);
 
-		     
-	$finish;
-     end
+  ///////////////////////////////////////////////////////////////////////
+  $info("T2: Read-Write Test");
+  ///////////////////////////////////////////////////////////////////////
+  for (unsigned int i = 0; i < 16; ++i)
+  begin
+    patterns[i] = $urandom;
+    rs_in[11:8] = i;
+    rs_in[7:4] = i;
+    rs_in[3:0] = i;
+    d_in = patterns[i];
+    @(posedge clk);
+  end
+
+  for (unsigned int i = 0; i < 16; ++i)
+  begin
+    d_in = '0;
+    rw_in = '0;
+    @(posedge clk);
+    assert (patterns[i] == a_out)
+    else   $error;
+    @(posedge clk);
+  end
+
+  for (unsigned int i = 0; i < 16; ++i)
+  begin
+    d_in = '0;
+    rw_in = '0;
+    @(posedge clk);
+    assert (patterns[i] == b_out)
+    else   $error;
+    @(posedge clk);
+  end
+
+  $finish;
+    end
 endprogram
